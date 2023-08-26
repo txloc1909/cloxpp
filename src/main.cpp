d0af5cf31f779a3bf3ac66450cdfc809870a23d3
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-08-26 21:54:30 +0700

    split the interpreter to a class

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,57 @@
+#include <cstdlib>
 #include <iostream>
 #include <iterator>
 #include <fstream>
 #include <filesystem>
 #include <string>
 
-void run(std::string source);
-void runFile(const char* path);
-void runPrompt();
+
+class TreewalkInterpreter {
+    private:
+        bool hadError = false;
+        bool hadRuntimeError = false;
+    public:
+        void run(std::string source);
+        void runFile(const char* path);
+        void runPrompt();
+};
+
 
 int main(int argc, char *argv[]) {
+    TreewalkInterpreter interpreter;
     if (argc > 2) {
         std::cerr << "Usage: jlox [script]";
-        return 64;
+        std::exit(64);
     } else if (argc == 2) {
-        runFile(argv[1]);
+        interpreter.runFile(argv[1]);
     } else {
-        runPrompt();
+        interpreter.runPrompt();
     }
 }
 
-void runFile(const char* path) {
+void TreewalkInterpreter::runFile(const char* path) {
     std::ifstream ifs(path, std::ios::in | std::ios::binary);
     if (!ifs) {
         throw std::ios_base::failure("File does not exist!");
     }
     std::string source(std::istreambuf_iterator<char>{ifs}, {});
     run(source);
+
+    if (this->hadError) {
+        std::exit(65);
+    }
 }
 
-void run(std::string source) {
+void TreewalkInterpreter::run(std::string source) {
     std::cout << source << std::endl;
 }
 
-void runPrompt() {
+void TreewalkInterpreter::runPrompt() {
     std::string line;
     while(std::cin) {
         std::cout << ">>> ";
         std::getline(std::cin, line);
         run(line);
+        this->hadError = false;
     }
 }
