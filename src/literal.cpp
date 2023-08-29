40b26c23e93991a1853837c835c101c3b85f044a
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2023-08-30 00:45:52 +0700

    template metaprogramming go br br...

--- src/literal.cpp
+++
@@ -1,8 +0,0 @@
-#include "literal.hpp"
-
-std::ostream& operator<<(std::ostream &os, const Literal& literal) {
-    if (literal.has_value()) {
-        std::visit([&os](auto&& arg) { os << arg; }, literal.value());
-    }
-    return os;
-}
--- src/literal.hpp
+++ src/literal.hpp
@@ -1,11 +1,18 @@
 #ifndef CLOXPP_LITERAL_H
 #define CLOXPP_LITERAL_H
 
+#include <ostream>
 #include <optional>
 #include <variant>
-#include <string>
 
 using Literal = std::optional<std::variant<std::string, double, bool>>;
-std::ostream& operator<<(std::ostream &os, const Literal& literal);
+
+template<typename T, typename... Ts>
+std::ostream& operator<<(std::ostream& os, const std::optional<std::variant<T, Ts...>>& v) {
+    if (v.has_value()) {
+        std::visit([&os](auto&& arg) { os << arg; }, v.value());
+    }
+    return os;
+}
 
 #endif // !CLOXPP_LITERAL_H
