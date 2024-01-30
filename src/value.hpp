efedf67928ceb2f498255a4987b671d6045d675d
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2024-01-30 22:17:19 +0700

    Revert "offload truthiness and equality from the Interpreter to the `Value` type"
    Causes weird ASan bug. Not worth it.
    
    This reverts commit 85dff024f099081dac78e822f0ecaa1c70e76c76.

--- src/value.hpp
+++ src/value.hpp
@@ -8,17 +8,9 @@
 using Literal = std::variant<std::monostate, std::string, double, bool>;
 
 // Literal is a subset of Value
-class Value : public std::variant<std::monostate, std::string, double, bool> {
-private:
-    using variant::variant;
-
-public:
-    bool isTruthy() const;
-};
+using Value = std::variant<std::monostate, std::string, double, bool>;
 
 std::string stringify(const Literal &value);
 std::ostream &operator<<(std::ostream &os, const Literal &literal);
-
-Value valueFromLiteral(const Literal &literal);
 
 #endif // !CLOXPP_VALUE_H
