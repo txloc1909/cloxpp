eca3230c249c79d63850274a7f367d4aa70e9a74
Tran Xuan Loc <tranxuanloc19920@gmail.com>
2024-03-24 17:31:00 +0700

    minor modification of string concatenation

--- src/object.cpp
+++ src/object.cpp
@@ -33,9 +33,9 @@ ObjString ObjString::concatenate(const ObjString &str1, const ObjString &str2) {
     int length = str1.size() + str2.size();
     char *chars = ALLOCATE(char, length + 1);
 
-    std::copy(str1.data(), str1.data() + str1.size(), chars);
-    std::copy(str2.data(), str2.data() + str2.size(), chars + str1.size());
-    chars[length] = '\0';
+    auto *mid = std::copy(str1.data(), str1.data() + str1.size(), chars);
+    auto *end = std::copy(str2.data(), str2.data() + str2.size(), mid);
+    *end = '\0';
 
     return ObjString(chars, length);
 }
